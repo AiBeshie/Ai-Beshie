7c5a75097a596ef4e458f2c3e74db0182be9ab53
AiBeshie <niragoals@gmail.com>
2025-12-18 17:52:30 +0800

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,8 +1,8 @@
 // ------------------ PLAYER INIT ------------------
 if (!window.player) {
   window.player = {
-    level: 50,
-    coins: 50,
+    level: 2,
+    coins: 250,
     stardust: 500,
     party: [],
     items: {},
@@ -553,6 +553,7 @@ async function catchPokemon(ballType = "pokeball", onComplete = null) {
   isCatching = false;
   if (onComplete) onComplete();
 }
+
 
 
 
